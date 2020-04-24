8f5f4a9b6966036683d42aaf783b9ee6d1687353
dammyayans <dammyayans560@gmail.com>
2020-04-23 19:53:11 -0700

    still working on the navbar

--- src/App.js
+++ src/App.js
@@ -1,140 +1,122 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import storage from 'local-storage-fallback';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import storage from "local-storage-fallback";
 
-import './App.css';
-import './components/Navbar/Navbar.css';
+import "./App.css";
+import "./components/Navbar/Navbar.css";
 
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
 
-import Logo from './images/logo.png';
-import MenuBar from './images/menuicon.png';
-import DarkModeIcon from './images/darkmodeicon.png';
+import Logo from "./images/logo.png";
+import MenuBar from "./images/menuicon.png";
+import DarkModeIcon from "./images/darkmodeicon.png";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
-import Footer from './components/Footer/Footer';
+import Home from "./Pages/Home";
+import TheTeam from "./Pages/TheTeam";
+import OurWorks from "./Pages/OurWorks";
+import Confirmation from "./Pages/Confirmation";
+import Subscription from "./Pages/Subscription";
 
-import Home from './Pages/Home';
-import TheTeam from './Pages/TheTeam';
-import OurWorks from './Pages/OurWorks';
-import Confirmation from './Pages/Confirmation';
-import Subscription from './Pages/Subscription';
-
-import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 // import Confirmation from './Pages/sub';
 // import Response from './Pages/Response';
 
-const GlobalStyle = createGlobalStyle `
+const GlobalStyle = createGlobalStyle`
 
 body{
-  background-color: ${props => props.theme.mode === 'dark' ? '#111' : '#EEE'};
-  color: ${props => props.theme.mode === 'dark' ? '#FFF' : '#343434'};
+  background-color: ${(props) =>
+    props.theme.mode === "dark" ? "#111" : "#EEE"};
+  color: ${(props) => (props.theme.mode === "dark" ? "#FFF" : "#343434")};
 }
 .cover{
-  color: ${props => props.theme.mode === 'dark' ? '#EEE' : '#076847'};
+  color: ${(props) => (props.theme.mode === "dark" ? "#EEE" : "#076847")};
 }
 .blackText{
-  color: ${props => props.theme.mode === 'dark' ? '#fff' : '#343434'};
+  color: ${(props) => (props.theme.mode === "dark" ? "#fff" : "#343434")};
 }
 .whitebg{
-  background-color: ${props => props.theme.mode === 'dark' ? '#111' : '#FFF'};
+  background-color: ${(props) =>
+    props.theme.mode === "dark" ? "#111" : "#FFF"};
 }
 .whitebgNav{
-  background-color: ${props => props.theme.mode === 'dark' ? '#000' : '#FFF'};
+  background-color: ${(props) =>
+    props.theme.mode === "dark" ? "#000" : "#FFF"};
 }
 .greybg{
-  background-color: ${props => props.theme.mode === 'dark' ? '#111' : '#F3F3F3'};
+  background-color: ${(props) =>
+    props.theme.mode === "dark" ? "#111" : "#F3F3F3"};
 }
 .heroheroL{
-  display: ${props => props.theme.mode === 'dark' ? 'none' : 'block'};
+  display: ${(props) => (props.theme.mode === "dark" ? "none" : "block")};
 }
 .heroheroD{
-  display: ${props => props.theme.mode === 'dark' ? 'block' : 'none'};
+  display: ${(props) => (props.theme.mode === "dark" ? "block" : "none")};
 }
 .a-get-started-testimonies img{
-  border: ${props => props.theme.mode === 'dark' ? '7px solid #343434' : '7px solid #fff'};
+  border: ${(props) =>
+    props.theme.mode === "dark" ? "7px solid #343434" : "7px solid #fff"};
 }
 .toggleButton{
-  background-color: ${props => props.theme.mode === 'dark' ? '#343434' : '#000'};   
-  color: ${props => props.theme.mode === 'dark' ? '#000' : '#343434'};   
+  background-color: ${(props) =>
+    props.theme.mode === "dark" ? "#343434" : "#000"};   
+  color: ${(props) => (props.theme.mode === "dark" ? "#000" : "#343434")};   
 }
 .toggleMode{
-  opacity: ${props => props.theme.mode === 'dark' ? '.5' : '1'};     
+  opacity: ${(props) => (props.theme.mode === "dark" ? ".5" : "1")};     
 }
 `;
 function getInitialTheme() {
-  const savedTheme = storage.getItem('theme')
-  return savedTheme ? JSON.parse(savedTheme) : { mode: 'light' }
+  const savedTheme = storage.getItem("theme");
+  return savedTheme ? JSON.parse(savedTheme) : { mode: "light" };
 }
 
 export default function App() {
-  const [theme, setTheme] = useState(getInitialTheme)
-    useEffect(()=> {
-        storage.setItem('theme', JSON.stringify(theme))
-      }, 
-      [theme]
-    );
-    
-    return (
+  const [theme, setTheme] = useState(getInitialTheme);
+  useEffect(() => {
+    storage.setItem("theme", JSON.stringify(theme));
+  }, [theme]);
+
+  return (
     <Router>
-      <ThemeProvider theme= { theme }>
-          <>
-            <div className='cover'>
-              <nav className="a-navbar-wrapper sticky-top whitebgNav">
-                  <Container>
-                      <nav>
-                          <span className="a-navbar-logo-container">
-                              <a href="/" ><img src={Logo} alt=""/></a>
-                          </span>
-                          <span className="a-navbar-others">
-                              <span className="a-navbar-dm">
-                                  {/* <img src={DarkModeIcon} alt="MenuBar"/>  */}
-                                  <img 
-                                    src={DarkModeIcon} 
-                                    alt="MenuBar" 
-                                    className="toggleMode"
-                                    onClick={ e => setTheme(
-                                    theme.mode === 'dark' 
-                                    ? {mode: 'light'}
-                                    : {mode: 'dark'})} /> 
-                              </span>
-                              <span className="a-navbar-menubar blackText">
-                                  <img src={MenuBar} alt="MenuBar" />
-                                  <span>Menu</span>
-                              </span>
-                          </span>
-                      </nav>
-                  </Container>
-                </nav>
-              
-                <Switch>
-                  
-                  <Route exact path="/Subscription">
-                    <Subscription />
-                  </Route>
+      <ThemeProvider theme={theme}>
+        <>
+          <div className="cover">
+            <Navbar
+              onClick={() =>
+                setTheme(
+                  theme.mode === "dark" ? { mode: "light" } : { mode: "dark" }
+                )
+              }
+            />
+            <Switch>
+              <Route exact path="/Subscription">
+                <Subscription />
+              </Route>
 
-                  <Route exact path="/Confirmation">
-                    <Confirmation />
-                  </Route>
+              <Route exact path="/Confirmation">
+                <Confirmation />
+              </Route>
 
-                  <Route exact path="/OurWorks">
-                    <OurWorks />
-                  </Route>
-                  
-                  <Route exact path="/TheTeam">
-                    <TheTeam />
-                  </Route>
+              <Route exact path="/OurWorks">
+                <OurWorks />
+              </Route>
 
-                  {/* <Route component={ErrorPage} /> */}
+              <Route exact path="/TheTeam">
+                <TheTeam />
+              </Route>
 
-                  <Route exact path="/">
-                    <Home />
-                  </Route>
+              {/* <Route component={ErrorPage} /> */}
 
-                </Switch>
-              <Footer /> 
-              <GlobalStyle />
-            </div>
-          </>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </Switch>
+            <Footer />
+            <GlobalStyle />
+          </div>
+        </>
       </ThemeProvider>
     </Router>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import './Navbar.css';
-import { Container } from 'react-bootstrap';
+import "./Navbar.css";
+import { Container } from "react-bootstrap";
 
-import Logo from '../../images/logo.png';
-import MenuBar from '../../images/menuicon.png';
-import DarkModeIcon from '../../images/darkmodeicon.png';
+import Logo from "../../images/logo.png";
+import MenuBar from "../../images/menuicon.png";
+import DarkModeIcon from "../../images/darkmodeicon.png";
 class Navbar extends Component {
-    render() {
-        return (
-            <nav className="a-navbar-wrapper sticky-top whitebgNav">
-                <Container>
-                    <nav>
-                        <span className="a-navbar-logo-container">
-                            <a href="/" ><img src={Logo} alt=""/></a>
-                        </span>
-                        <span className="a-navbar-others">
-                            <span className="a-navbar-dm">
-                                <img src={DarkModeIcon} alt="MenuBar"/> 
-                            </span>
-                            <span className="a-navbar-menubar blackText">
-                                <img src={MenuBar} alt="MenuBar" />
-                                <span>Menu</span>
-                            </span>
-                            <span>
-                            {/* <button 
-                                onClick={ e => setTheme(
-                                    theme.mode === 'dark' 
-                                    ? {mode: 'light'}
-                                    : {mode: 'dark'})}>
-                                    Toggle Theme
-                                </button> */}
-                            </span>
-                        </span>
-                    </nav>                    
-                </Container>
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <nav className="a-navbar-wrapper sticky-top whitebgNav">
+        <Container>
+          <nav>
+            <span className="a-navbar-logo-container">
+              <a href="/">
+                <img src={Logo} alt="" />
+              </a>
+            </span>
+            <span className="a-navbar-others">
+              <span className="a-navbar-dm">
+                {/* <img src={DarkModeIcon} alt="MenuBar"/>  */}
+                <img
+                  src={DarkModeIcon}
+                  alt="MenuBar"
+                  className="toggleMode"
+                  onClick={this.props.onClick}
+                />
+              </span>
+              <span className="a-navbar-menubar blackText">
+                <img src={MenuBar} alt="MenuBar" />
+                <span>Menu</span>
+              </span>
+            </span>
+          </nav>
+        </Container>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
